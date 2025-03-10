64b2f56aa85b4206e53380efd1daa3114c19e7e9
rhazeslip <rhazeslip@cub.uca.edu>
2025-03-09 19:27:52 -0500

    Fixing errors

--- src/DataProcessing.java
+++ src/DataProcessing.java
@@ -1,24 +1,33 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DataProcessing {
     public static List<Country> readCountry(String filePath) throws IOException {
+        List<Country> countries = new ArrayList<>();
         try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
-            return lines.skip(1)
-                    .map(line -> {
-                        String[] parts = line.split(",");
-                        return new Country(
-                            parts[0], //Country Name
-                            Integer.parseInt(parts[1]), //year
-                            Double.parseDouble(parts[2]), //Arable land percentage
-                            Double.parseDouble(parts[3]), //Forest percentage
-                            Double.parseDouble(parts[4])  //crop percentage
-                    );
-            }).collect(Collectors.toList());
+            lines.skip(1).forEach(line -> {
+                String[] parts = line.split(",");
+                try {
+                    String countryName = parts[0];
+                    int year = Integer.parseInt(parts[1].trim());
+                    double arableLandPercentage = Integer.parseInt(parts[2].trim());
+                    double forestPercentage = Integer.parseInt(parts[3].trim());
+                    double cropPercentage = Integer.parseInt(parts[4].trim());
+
+                    countries.add(new Country(countryName, year, arableLandPercentage, forestPercentage,
+                                    cropPercentage));
+                } catch (NumberFormatException e) {
+                    System.err.println("Skipping invalid row: " + line);
+                    e.printStackTrace();
+                }
+
+            });
         }
+        return countries;
     }
 }
